95af131610bd591d0c731dc4866d2d925b4c1713
Christopher Dick <e0946375@student.tuwien.ac.at>
2015-10-22 14:31:37 +0200

    changed basic script and added example image

--- src/detectCard.m
+++ src/detectCard.m
@@ -1,8 +1,8 @@
 
 function detectCard(filename)
 % DETECTCARD Detects playing cards in an image and prints their values.
-%   DETECTCARD(FILENAME)    reads the image specified by FILENAME 
-%                           in either .png, .tiff or .jpeg format 
+%   DETECTCARD(FILENAME)    reads the image specified by FILENAME
+%                           in either .png, .tiff or .jpeg format
 %                           as an input and prints the card values as
 %                           textual output to the console.
 
@@ -13,7 +13,7 @@ clc
 % Read in the image file and convert to grey scale
 if ~exist(filename, 'file');
     sprinf(['The specified file does not exist within MATLAB environment.\n'...
-            'Please check your input again.'])
+        'Please check your input again.'])
     return
 end
 
@@ -25,30 +25,49 @@ end
 
 % check if read is intensity image, convert if not
 if ndims(original) > 2;
-   greyIm = rgb2gray(original);
+    greyIm = rgb2gray(original);
 else
     greyIm = original;
 end
 
-% Create a binary image by thresholding against white (card background)
+%{
+ Create a binary image by thresholding against white (card background)
 thld = max(max(greyIm));    % find strongest white in picture
 epsilon = 0.15;             % range to define the threshold
 binaryIm = false(size(greyIm));
 binaryIm(greyIm > thld - epsilon) = 1;
+%}
 
-% fill holes in cards caused by patternsrel
-SE = strel('square', 10);
-closedIm = imclose(binaryIm, SE);
-figure;
-imshow(binaryIm);
+% Create a binary image by threholding with Otsu against background
+thld = graythresh(greyIm);
+epsilon = 0.15;
+binaryIm = false(size(greyIm));
+binaryIm(greyIm > thld + epsilon) = 1;
+
+
+% fill holes in cards caused by patterns
+% SE = strel('square', 10);
+% closedIm = imclose(binaryIm, SE);
+closedIm = bwmorph(binaryIm, 'thicken', 10);
+%figure;
+%imshow(closedIm);
 
 
 %% Segmentation of the cards
 
 % Detect cards by connecte component labeling
 
-[labels, numCards] = bwlabel(closedIm);
-imshow(labels);
-numCards
+[labels, numCards] = bwlabel(binaryIm, 8);
+%imshow(labels);
+%numCards
+
+
+%% Value recognition of segmented card
+
+singleCard = imread('Example_Input_Single.jpeg');
+greySingle = rgb2gray(singleCard);
+txt = ocr(greySingle);
+txt
+
 
 end
